558cb63ee61bc27b9a7e3cd8d2cb0e3dc0be4275
Zach Reed <tzreedjr@gmail.com>
2015-04-06 11:06:12 -0400

    updated Map to use entrySet for loop; also refactored code to reference final strings and ints for network sockets

--- app/src/main/java/tool_kit/gymanji/com/toolkit/MainActivity.java
+++ app/src/main/java/tool_kit/gymanji/com/toolkit/MainActivity.java
@@ -15,9 +15,9 @@ import android.widget.TextView;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -25,52 +25,66 @@ public class MainActivity extends ActionBarActivity {
     private TextView tvConnectedNetwork, tvApple_phobos443, tvApple_phobos80, tvApple_courier5223, tvApple_courier443, tvApple_ocsp443, tvApple_ocsp80,
             tvApple_itunes443, tvApple_itunes80, tvAndroid_mtalk5228, tvAndroid_play443, tvWindows_net443, tvWindows_com443;
     private Button btnConnectivityTester;
-    private final String nonWiFi = "Not connected to WiFi";
-    private final String unknown = "<unknown ssid>";
+    private final String NON_WIFI = "Not connected to WiFi", UNKNOWN = "<unknown ssid>", PHOBOS = "phobos.apple.com", COURIER = "1-courier.apple.com",
+            OCSP = "ocsp.apple.com", ITUNES = "ax.itunes.apple.com", MTALK = "mtalk.google.com", PLAY = "play.google.com", NOTIFY_NET = "s.notify.live.net";
+    private final int EIGHTY = 80, SSL = 443, FIVETWOTWOTHREE = 5223, FIVETWOTWOEIGHT = 5228;
     private Socket socket;
     boolean returnedBoolFromOnPostExecute;
     Map networkEndPointsById;
 
-    public class NetworkObject {
-        private String destAddr;
-        private int port;
-        private TextView textView;
 
-        public NetworkObject(String destAddr, int port, TextView textView) {
-            this.destAddr = destAddr;
-            this.port = port;
-            this.textView = textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Locating currently connected WiFi network
+        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        String currentWiFi = wifiInfo.getSSID();
+        String currentWiFi_noQuotes = currentWiFi.substring(1, currentWiFi.length() - 1);
+        Log.d("SSID", currentWiFi);
+        Log.d("rBFOPtE - onCreate", returnedBoolFromOnPostExecute ? "true" : "false");
+
+        // Hooking up TextViews for network connection results
+        tvConnectedNetwork = (TextView) findViewById(R.id.tvConnectedNetwork);
+        tvApple_phobos443 = (TextView) findViewById(R.id.tvApple_phobos443);
+        tvApple_phobos80 = (TextView) findViewById(R.id.tvApple_phobos80);
+        tvApple_courier5223 = (TextView) findViewById(R.id.tvApple_courier5223);
+        tvApple_courier443 = (TextView) findViewById(R.id.tvApple_courier443);
+        tvApple_ocsp443 = (TextView) findViewById(R.id.tvApple_ocsp443);
+        tvApple_ocsp80 = (TextView) findViewById(R.id.tvApple_ocsp80);
+        tvApple_ocsp443 = (TextView) findViewById(R.id.tvApple_ocsp443);
+        tvApple_itunes443 = (TextView) findViewById(R.id.tvApple_itunes443);
+        tvApple_itunes80 = (TextView) findViewById(R.id.tvApple_itunes80);
+        tvAndroid_mtalk5228 = (TextView) findViewById(R.id.tvAndroid_mtalk5228);
+        tvAndroid_play443 = (TextView) findViewById(R.id.tvAndroid_play443);
+        tvWindows_net443 = (TextView) findViewById(R.id.tvWindows_net443);
+        tvWindows_com443 = (TextView) findViewById(R.id.tvWindows_com443);
+
+        // Update TextView depending on network connection type
+        if (currentWiFi == UNKNOWN) {
+            tvConnectedNetwork.setText(NON_WIFI);
+        } else {
+            tvConnectedNetwork.setText(currentWiFi_noQuotes);
         }
 
-        public String getDestAddr() {
-            return destAddr;
-        }
+        class NetworkHashTable {
+            public void main(String[] args) {
 
-        public int getPort() {
-            return port;
-        }
+                Map networkEndPointsById = new HashMap<Integer, NetworkObject>();
 
-        public TextView getTextView() {
-            return textView;
-        }
-    }
-
-    public class NetworkHashTable {
-        public void main(String[] args) {
-
-            Map networkEndPointsById = new HashMap<Integer, NetworkObject>();
-
-            networkEndPointsById.put(1, new NetworkObject("phobos.apple.com", 443, tvApple_phobos443));
-            networkEndPointsById.put(2, new NetworkObject("phobos.apple.com", 80, tvApple_phobos80));
-            networkEndPointsById.put(3, new NetworkObject("1-courier.apple.com", 5223, tvApple_courier5223));
-            networkEndPointsById.put(4, new NetworkObject("1-courier.apple.com", 443, tvApple_courier443));
-            networkEndPointsById.put(5, new NetworkObject("ocsp.apple.com", 443, tvApple_ocsp443));
-            networkEndPointsById.put(6, new NetworkObject("ocsp.apple.com", 80, tvApple_ocsp80));
-            networkEndPointsById.put(7, new NetworkObject("ax.itunes.apple.com", 443, tvApple_itunes443));
-            networkEndPointsById.put(8, new NetworkObject("ax.itunes.apple.com", 80, tvApple_itunes80));
-            networkEndPointsById.put(9, new NetworkObject("mtalk.google.com", 5228, tvAndroid_mtalk5228));
-            networkEndPointsById.put(10, new NetworkObject("play.google.com", 443, tvAndroid_play443));
-            networkEndPointsById.put(10, new NetworkObject("s.notify.live.net", 443, tvWindows_net443));
+                networkEndPointsById.put(1, new NetworkObject(PHOBOS, SSL, tvApple_phobos443));
+                networkEndPointsById.put(2, new NetworkObject(PHOBOS, EIGHTY, tvApple_phobos80));
+                networkEndPointsById.put(3, new NetworkObject(COURIER, FIVETWOTWOTHREE, tvApple_courier5223));
+                networkEndPointsById.put(4, new NetworkObject(COURIER, SSL, tvApple_courier443));
+                networkEndPointsById.put(5, new NetworkObject(OCSP, SSL, tvApple_ocsp443));
+                networkEndPointsById.put(6, new NetworkObject(OCSP, EIGHTY, tvApple_ocsp80));
+                networkEndPointsById.put(7, new NetworkObject(ITUNES, SSL, tvApple_itunes443));
+                networkEndPointsById.put(8, new NetworkObject(ITUNES, EIGHTY, tvApple_itunes80));
+                networkEndPointsById.put(9, new NetworkObject(MTALK, FIVETWOTWOEIGHT, tvAndroid_mtalk5228));
+                networkEndPointsById.put(10, new NetworkObject(PLAY, SSL, tvAndroid_play443));
+                networkEndPointsById.put(11, new NetworkObject(NOTIFY_NET, SSL, tvWindows_net443));
 
 //            NetworkObject Apple1 = new NetworkObject("phobos.apple.com", 443, tvApple_phobos443);
 //            NetworkObject Apple2 = new NetworkObject("phobos.apple.com", 80, tvApple_phobos80);
@@ -96,55 +110,23 @@ public class MainActivity extends ActionBarActivity {
 //            network_endpoints.put("play.google.com", 443);
 //            network_endpoints.put("s.notify.live.net", 443);
 //            network_endpoints.put("notify.windows.com", 443);
-        }
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // Locating currently connected WiFi network
-        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
-        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-        String currentWiFi = wifiInfo.getSSID();
-        String currentWiFi2 = currentWiFi.substring(1, currentWiFi.length() - 1);
-        Log.d("SSID", currentWiFi);
-        Log.d("rBFOPtE - onCreate", returnedBoolFromOnPostExecute ? "true" : "false");
-
-        // Hooking up TextViews to update based on network connection results
-        tvConnectedNetwork = (TextView) findViewById(R.id.tvConnectedNetwork);
-        tvApple_phobos443 = (TextView) findViewById(R.id.tvApple_phobos443);
-        tvApple_phobos80 = (TextView) findViewById(R.id.tvApple_phobos80);
-        tvApple_courier5223 = (TextView) findViewById(R.id.tvApple_courier5223);
-        tvApple_courier443 = (TextView) findViewById(R.id.tvApple_courier443);
-        tvApple_ocsp443 = (TextView) findViewById(R.id.tvApple_ocsp443);
-        tvApple_ocsp80 = (TextView) findViewById(R.id.tvApple_ocsp80);
-        tvApple_ocsp443 = (TextView) findViewById(R.id.tvApple_ocsp443);
-        tvApple_itunes443 = (TextView) findViewById(R.id.tvApple_itunes443);
-        tvApple_itunes80 = (TextView) findViewById(R.id.tvApple_itunes80);
-        tvAndroid_mtalk5228 = (TextView) findViewById(R.id.tvAndroid_mtalk5228);
-        tvAndroid_play443 = (TextView) findViewById(R.id.tvAndroid_play443);
-        tvWindows_net443 = (TextView) findViewById(R.id.tvWindows_net443);
-        tvWindows_com443 = (TextView) findViewById(R.id.tvWindows_com443);
-
-        // Update TextView depending on network connection type
-        if (currentWiFi == unknown) {
-            tvConnectedNetwork.setText(nonWiFi);
-        } else {
-            tvConnectedNetwork.setText(currentWiFi2);
+            }
         }
 
         // Button listener and invocation of asyncTask Socket
         btnConnectivityTester = (Button) findViewById(R.id.btnConnectivityTester);
         btnConnectivityTester.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Log.d("onClick", "onClick just started");
 
-                Collection<NetworkObject> networkObjects = networkEndPointsById.values();
+//                Collection<NetworkObject> networkObjects = networkEndPointsById.values();
+                Set<Map.Entry<Integer, NetworkObject>> entrySet = networkEndPointsById.entrySet();
 
-                for (NetworkObject networkobject : networkObjects) {
+                for (Map.Entry<Integer, NetworkObject> entry : entrySet) {
+
+                    NetworkObject networkobject = entry.getValue();
 
                     ToolKitSocket donkey = new ToolKitSocket(networkobject.getDestAddr(), networkobject.getPort());
                     Log.d("onClick", "new socket just created");
@@ -163,6 +145,30 @@ public class MainActivity extends ActionBarActivity {
                 }
             }
         });
+    }
+
+    public class NetworkObject {
+        private String destAddr;
+        private int port;
+        private TextView textView;
+
+        public NetworkObject(String destAddr, int port, TextView textView) {
+            this.destAddr = destAddr;
+            this.port = port;
+            this.textView = textView;
+        }
+
+        public String getDestAddr() {
+            return destAddr;
+        }
+
+        public int getPort() {
+            return port;
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
     }
 
     // Network test connections using Socket
