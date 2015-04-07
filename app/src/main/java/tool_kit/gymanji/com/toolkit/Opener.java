f040dd2838275e3387d4d91688cacb8e5413099f
Zach Reed <tzreedjr@gmail.com>
2015-04-07 18:04:43 -0400

    created new Activity and set it as main file to open app

---
+++ app/src/main/java/tool_kit/gymanji/com/toolkit/Opener.java
@@ -0,0 +1,48 @@
+package tool_kit.gymanji.com.toolkit;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Opener extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opener);
+    }
+
+    public void openMain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        Button changeScreens = (Button) findViewById(R.id.btnConnectivityTester);
+        startActivity(intent);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_opener, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
